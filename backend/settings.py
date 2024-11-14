22c2214797510daff5ee4972260e5b994b305f40
aberintan <aberintan@gmail.com>
2024-11-14 16:20:21 -0500

    Update settings.py to use environment variables and add .env file

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,12 +78,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # Use 'django.db.backends.postgresql' for PostgreSQL
-        'NAME': 'personal_finance_tracker',          # Your database name
-        'USER': 'myuser',                            # Your PostgreSQL username
-        'PASSWORD': 'mypassword',                    # Your PostgreSQL password
-        'HOST': 'localhost',                         # Use 'localhost' for local development
-        'PORT': '5432',                              # Default port for PostgreSQL
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
