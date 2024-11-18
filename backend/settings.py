59b482dec29370cced8fb5d9c204bd42d790becf
aberintan <aberintan@gmail.com>
2024-11-18 14:05:14 -0500

    Config: Set custom user model in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,6 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -105,6 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Specify the custom user model to be used for authentication and user management
+AUTH_USER_MODEL = 'finance.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
